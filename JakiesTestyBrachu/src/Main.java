ee49ebdbdf3336a0f41f061ede858fe1b7795442
Mabi671 <karol.wladzinski@gmail.com>
2024-06-11 18:26:27 +0200

    Commit 1.1

--- JakiesTestyBrachu/src/Main.java
+++ JakiesTestyBrachu/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (int i = 1; i <= 225; i++) {
+        for (int i = 1; i <= 128; i++) {
             System.out.println("i = " + i);
         }
     }
