e596a9b6967213f13411c4f95d3848593e97e269
Mabi671 <karol.wladzinski@gmail.com>
2024-06-11 18:58:57 +0200

    Commit 1.1

--- JakiesTestyBrachu/src/Main.java
+++ JakiesTestyBrachu/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        for (int i = 1; i <= 128; i++) {
-            System.out.println("i = " + i);
+        String binaryString = "01001110";
+        int wynik = 0;
+        for (int i = 7; i > 0; i--) {
+            char har = binaryString.charAt(i);
+            int num = Integer.parseInt(String.valueOf(har));
+            wynik += (int) (Math.pow(2, i) * num);
         }
+        System.out.println(wynik);
     }
 }
