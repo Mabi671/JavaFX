1d29f68f1b7ea47f8e8f47ad60bca7dd14b39d6a
Mabi671 <karol.wladzinski@gmail.com>
2024-06-12 23:09:47 +0200

    Commit 1.2

--- JakiesTestyBrachu/src/Main.java
+++ JakiesTestyBrachu/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,5 +11,29 @@ public class Main {
             wynik += (int) (Math.pow(2, i) * num);
         }
         System.out.println(wynik);
+        int number = 114;
+        List<Integer> squares = new ArrayList<>();
+        int x = -1;
+        StringBuilder sb = new StringBuilder();
+        while(Math.pow(2, x) < number){
+            x += 1;
+            squares.add((int) Math.pow(2, x));
+            sb.append(0);
+        }
+        String answer = sb.toString();
+        while (number > 0)
+        {
+            for(int i = 0; i < squares.size(); i++){
+                if((squares.get(i) * 2) > number)
+                {
+                    number -= squares.get(i);
+                    answer = answer.substring(0, i)
+                            + "1"
+                            + answer.substring(i + 1);
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
     }
 }
